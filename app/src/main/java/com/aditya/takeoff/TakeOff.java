5905cb9f3c272204f3cac828571ec0c18c9a3568
Mr4D <a.prabhakara96@gmail.com>
2017-09-03 21:11:17 +1000

    Create intent to main activity.

--- app/src/main/java/com/aditya/takeoff/TakeOff.java
+++ app/src/main/java/com/aditya/takeoff/TakeOff.java
@@ -1,13 +1,40 @@
 package com.aditya.takeoff;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class TakeOff extends AppCompatActivity {
+
+    Button loginBtn;
+    EditText usernameText;
+    EditText passwordText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_take_off);
+        loginBtn = (Button)findViewById(R.id.btLogin);
+        usernameText = (EditText)findViewById(R.id.username);
+        passwordText = (EditText)findViewById(R.id.password);
     }
+
+    public void login(View view) {
+        String username = usernameText.getText().toString();
+        if (!username.isEmpty()) {
+            Intent startIntent = new Intent(getApplicationContext(), main.class);
+            startIntent.putExtra("com.aditya.takeoff.USER_ID", username);
+            startActivity(startIntent);
+        }
+        else {
+            Toast.makeText(getApplicationContext(), "Please enter a username.", Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+
 }
--- app/src/main/java/com/aditya/takeoff/main.java
+++ app/src/main/java/com/aditya/takeoff/main.java
@@ -2,12 +2,20 @@ package com.aditya.takeoff;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class main extends AppCompatActivity {
-
+    TextView mainTestText;
+    String text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (getIntent().hasExtra("com.aditya.takeoff.USER_ID")) {
+            mainTestText = (TextView)findViewById(R.id.mainTestText);
+            text = getIntent().getExtras().getString("com.aditya.takeoff.USER_ID");
+            mainTestText.setText(text);
+        }
     }
 }

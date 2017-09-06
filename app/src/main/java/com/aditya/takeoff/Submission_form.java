4f241fae2c23f252c7d7fc834b4116e858eb078d
Mr4D <a.prabhakara96@gmail.com>
2017-09-06 17:10:40 +1000

    Improve GPS functionallity

--- app/src/main/java/com/aditya/takeoff/Submission_form.java
+++ app/src/main/java/com/aditya/takeoff/Submission_form.java
@@ -18,6 +18,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
 
 
 
@@ -28,6 +32,7 @@ public class Submission_form extends AppCompatActivity {
     LocationListener locationListener;
     TextView locationTextView;
 
+    // Will be used later for
     Double longitude;
     Double latitude;
 
@@ -40,86 +45,69 @@ public class Submission_form extends AppCompatActivity {
         locationTextView = (TextView)findViewById(R.id.locationTextView);
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
-        locationListener = new LocationListener() {
-            @Override
-            public void onLocationChanged(Location location) {
-
-                longitude = location.getLongitude();
-                latitude = location.getLatitude();
-                //Toast.makeText(getApplicationContext(), "Longitude: " + longitude + ", Latitude: " + latitude, Toast.LENGTH_LONG).show();
-                locationTextView.setText("Longitude: " + longitude + ", Latitude: " + latitude);
-            }
-
-            @Override
-            public void onStatusChanged(String s, int i, Bundle bundle) {
-
-            }
-
-            @Override
-            public void onProviderEnabled(String s) {
-
-            }
-
-            @Override
-            public void onProviderDisabled(String s) {
-
-                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                startActivity(i);
-            }
-        };
-        configure_button();
+        configureButton();
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         switch (requestCode) {
-            case 10:
-                configure_button();
-                break;
-            default:
-                break;
+            case 0:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    configureButton();
+                }
         }
     }
 
-    void configure_button() {
-        // first check for permissions
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
-                PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
-                                Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.INTERNET}
-                        , 10);
-            }
+    private void configureButton() {
+        submitBt.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View view) {
+                  createLocationRequest();
+              }
+          });
+    }
+
+
+    protected void createLocationRequest () {
+
+
+        LocationManager service = (LocationManager) getSystemService(LOCATION_SERVICE);
+        boolean enabled = service.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        if (!enabled) {
+            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(intent);
+        }
+
+
+
+
+
+
+
+        FusedLocationProviderClient mFusedLocationProviderClient;
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
             return;
         }
-        // this code won'textView execute IF permissions are not allowed, because in the line above there is return statement.
-        submitBt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //noinspection MissingPermission
-                locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
-            }
-        });
+        else {
+            mFusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if (location != null) {
+                        longitude = location.getLongitude();
+                        latitude = location.getLatitude();
+                        String locationMsg = "Latitude: " + latitude + "\n " + "Longitude: " + longitude;
+                        locationTextView.setText(locationMsg);
+
+                        // TODO -> If ran successfully till here pass this data into a database and use intent to go to the main.class.
+                    }
+                }
+            });
+        }
     }
 
 
 
 
-    public void submitTasks(View view) {
-
-        Toast.makeText(this, "Longitude: " + longitude + ", Latitude: " + latitude, Toast.LENGTH_LONG).show();
-
-
-
-        //Get location and toast it
-
-
-
-//        Intent startIntent = new Intent(getApplicationContext(), main.class);
-////        startIntent.putExtra("com.aditya.takeoff.USER_ID", username);
-////        startIntent.putExtra("com.aditya.takeoff.NFC_ID", NFC_ID);
-//        startActivity(startIntent);
-    }
 }
